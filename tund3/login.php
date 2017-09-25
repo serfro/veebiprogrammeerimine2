64d08b4dddf8492a1bc9c22bc73beb50993c1b4e
Sergei Frolov <serfro@users.noreply.github.com>
2017-09-25 09:22:15 +0300

    tund4

--- tund3/login.php
+++ tund3/login.php
@@ -6,6 +6,17 @@ $mySignupPassword = "";
 $mySignupName = "";
 $mySignupSurname ="";
 $mySignupGender = "";
+$signupBirthMonth ="";
+
+$monthNamesEt = ["Jaanuar", "Veebruar", "Märts" , "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"];
+
+//synnikuu valik
+$signupMonthSelectHTML="";
+$signupMonthSelectHTML .= '<select name="signupBirthMonth">\n';
+$signupMonthSelectHTML .='<option value="" selected disabled>Vali synnikuu</option> ."\n"';
+
+
+
 	if(isset($_POST["loginEmail"])){
 		$myLoginEmail = $_POST["loginEmail"];
 	}
@@ -24,11 +35,24 @@ $mySignupGender = "";
 	if(isset($_POST["signupSurName"])){
 		$mySignupSurname = $_POST["signupSurName"];
 	}
-	if(isset($_POST["gender"])){
-		$mySignupGender = $_POST["gender"];
+	if (isset($_POST["gender"]) && !empty($_POST["gender"])){ //kui on määratud ja pole tühi
+			$gender = intval($_POST["gender"]);
+		} else {
+			//$signupGenderError = " (Palun vali sobiv!) Määramata!";
+	}
+	if(isset($_POST["signupBirthMonth"])){
+		$signupBirthMonth = intval($_POST["signupBirthMonth"]);
 	}
 	
-	
+	foreach($monthNamesEt as $key=>$month){
+	if($key + 1 === $signupBirthMonth){
+		$signupMonthSelectHTML .= '<option value="' .($key + 1) .'" selected>' .$month .'</option> ."\n"';
+	}
+	else {
+		$signupMonthSelectHTML .= '<option value="' .($key + 1) .'">' .$month .'</option> ."\n"';
+	}
+}
+	$signupMonthSelectHTML .='</select> ';
 
 ?>
 
@@ -44,13 +68,15 @@ $mySignupGender = "";
 	<body>
 	
 	<form method="POST">
+		<label for="loginEmail">Login</label>
 		<input id="loginEmail" name="loginEmail" type="email">
+		<label for="loginPassword">Parool</label>
 		<input id="loginPassword" name="loginPassword" type="password">
 		<input id="submitEmail" name="submitEmail" id="submitPassword" name="submitPassword" type="submit" value="kinnita">
 		
 	</form>
 		
-	<p> Loome uut kasutajat:</p>
+	<h2> Uus kasutaja:</h2>
 	<p> Sisestage oma andmed</p>
 	<form method="POST">
 		<label for="signupFirstName">Nimi</label>
@@ -58,10 +84,12 @@ $mySignupGender = "";
 		<label for="signupSurName">Perekonnanimi</label>
 		<input name="signupSurName" type="text">
 		<p></p>
-		<label for="mees">Mees</label>
-		<input type="radio" name="gender" id="mees" value="1">
-		<label for="naine">Naine</label>
-		<input type="radio" name="gender" id="naine" value="2">
+		<label>Sugu</label><span>
+		<br>
+		<input type="radio" name="$mySignupGender" id="mees" value="1" <?php if ($mySignupGender == '1') {echo 'checked';} ?>><label>Mees</label> <!-- Kõik läbi POST'i on string!!! -->
+		<input type="radio" name="$mySignupGender" id="naine" value="2" <?php if ($mySignupGender == '2') {echo 'checked';} ?>><label>Naine</label>
+		<br><br>
+		
 		<?php
 		
 		?>
@@ -72,6 +100,10 @@ $mySignupGender = "";
 		<input name="signupPassword" type="password">
 		<p></p>
 		<input name="signupFirstName" name="signupSurName" name="gender" name="signupEmail" name="signupPassword" type="submit" value="Kinnita">
+		<label>Sisesta oma synnikuupaev</label>
+		<?php
+			echo$signupMonthSelectHTML;
+		?>
 	</form>
 	</body>
 </html>
